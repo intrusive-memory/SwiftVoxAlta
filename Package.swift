bca35228451995e15763f53ee5cbeb4cc33d2845
Tom Stovall <119924+stovak@users.noreply.github.com>
2026-05-03 09:45:45 -0700

    version bump dependencies (#28)
    Specifically not tagging this as a release. Because it's a no-op it will get carried as noise in the next release.

--- Package.swift
+++ Package.swift
@@ -10,107 +10,108 @@ import PackageDescription
 let useLocalSiblings = ProcessInfo.processInfo.environment["CI"] != "true"
 
 func sibling(_ name: String, remote: String, from version: Version) -> Package.Dependency {
-  let localPath = "../\(name)"
-  if useLocalSiblings && FileManager.default.fileExists(atPath: localPath) {
-    return .package(path: localPath)
-  }
-  return .package(url: remote, .upToNextMajor(from: version))
+    let localPath = "../\(name)"
+    if useLocalSiblings && FileManager.default.fileExists(atPath: localPath) {
+        return .package(path: localPath)
+    }
+    return .package(url: remote, .upToNextMajor(from: version))
 }
 
 let package = Package(
-  name: "SwiftVoxAlta",
-  platforms: [
-    .macOS(.v26),
-    .iOS(.v26),
-  ],
-  products: [
-    .library(
-      name: "SwiftVoxAlta",
-      targets: ["SwiftVoxAlta"]
-    ),
-    .executable(
-      name: "diga",
-      targets: ["diga"]
-    ),
-  ],
-  dependencies: [
-    sibling(
-      "SwiftHablare",
-      remote: "https://github.com/intrusive-memory/SwiftHablare.git",
-      from: "5.7.5"
-    ),
-    sibling(
-      "mlx-audio-swift",
-      remote: "https://github.com/intrusive-memory/mlx-audio-swift.git",
-      from: "0.5.1"
-    ),
-    sibling(
-      "SwiftAcervo",
-      remote: "https://github.com/intrusive-memory/SwiftAcervo.git",
-      from: "0.8.4"
-    ),
-    sibling(
-      "SwiftTuberia",
-      remote: "https://github.com/intrusive-memory/SwiftTuberia.git",
-      from: "0.6.0"
-    ),
-    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "1.7.1")),
-    sibling(
-      "vox-format",
-      remote: "https://github.com/intrusive-memory/vox-format.git",
-      from: "0.3.1"
-    ),
-  ],
-  targets: [
-    .target(
-      name: "SwiftVoxAlta",
-      dependencies: [
-        .product(name: "SwiftHablare", package: "SwiftHablare"),
-        .product(name: "MLXAudioTTS", package: "mlx-audio-swift"),
-        .product(name: "SwiftAcervo", package: "SwiftAcervo"),
-        .product(name: "Tuberia", package: "SwiftTuberia"),
-        .product(name: "VoxFormat", package: "vox-format"),
-      ],
-      swiftSettings: [
-        .enableUpcomingFeature("StrictConcurrency")
-      ]
-    ),
-    .executableTarget(
-      name: "diga",
-      dependencies: [
-        "SwiftVoxAlta",
-        .product(name: "ArgumentParser", package: "swift-argument-parser"),
-        .product(name: "SwiftAcervo", package: "SwiftAcervo"),
-      ],
-      path: "Sources/diga",
-      swiftSettings: [
-        .enableUpcomingFeature("StrictConcurrency")
-      ]
-    ),
-    .testTarget(
-      name: "SwiftVoxAltaTests",
-      dependencies: [
-        "SwiftVoxAlta",
-        .product(name: "SwiftAcervo", package: "SwiftAcervo"),
-        .product(name: "Tuberia", package: "SwiftTuberia"),
-        .product(name: "VoxFormat", package: "vox-format"),
-      ],
-      swiftSettings: [
-        .enableUpcomingFeature("StrictConcurrency")
-      ]
-    ),
-    .testTarget(
-      name: "DigaTests",
-      dependencies: [
-        "diga",
-        "SwiftVoxAlta",
-        .product(name: "ArgumentParser", package: "swift-argument-parser"),
-        .product(name: "SwiftAcervo", package: "SwiftAcervo"),
-        .product(name: "VoxFormat", package: "vox-format"),
-      ],
-      swiftSettings: [
-        .enableUpcomingFeature("StrictConcurrency")
-      ]
-    ),
-  ]
+    name: "SwiftVoxAlta",
+    platforms: [
+        .macOS(.v26),
+        .iOS(.v26),
+    ],
+    products: [
+        .library(
+            name: "SwiftVoxAlta",
+            targets: ["SwiftVoxAlta"]
+        ),
+        .executable(
+            name: "diga",
+            targets: ["diga"]
+        ),
+    ],
+    dependencies: [
+        sibling(
+            "SwiftHablare",
+            remote: "https://github.com/intrusive-memory/SwiftHablare.git",
+            from: "6.1.0"
+        ),
+        sibling(
+            "mlx-audio-swift",
+            remote: "https://github.com/intrusive-memory/mlx-audio-swift.git",
+            from: "0.6.0"
+        ),
+        sibling(
+            "SwiftAcervo",
+            remote: "https://github.com/intrusive-memory/SwiftAcervo.git",
+            from: "0.8.4"
+        ),
+        sibling(
+            "SwiftTuberia",
+            remote: "https://github.com/intrusive-memory/SwiftTuberia.git",
+            from: "0.6.0"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "1.7.1")),
+        sibling(
+            "vox-format",
+            remote: "https://github.com/intrusive-memory/vox-format.git",
+            from: "0.3.1"
+        ),
+    ],
+    targets: [
+        .target(
+            name: "SwiftVoxAlta",
+            dependencies: [
+                .product(name: "SwiftHablare", package: "SwiftHablare"),
+                .product(name: "MLXAudioTTS", package: "mlx-audio-swift"),
+                .product(name: "SwiftAcervo", package: "SwiftAcervo"),
+                .product(name: "Tuberia", package: "SwiftTuberia"),
+                .product(name: "VoxFormat", package: "vox-format"),
+            ],
+            swiftSettings: [
+                .enableUpcomingFeature("StrictConcurrency")
+            ]
+        ),
+        .executableTarget(
+            name: "diga",
+            dependencies: [
+                "SwiftVoxAlta",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "SwiftAcervo", package: "SwiftAcervo"),
+            ],
+            path: "Sources/diga",
+            swiftSettings: [
+                .enableUpcomingFeature("StrictConcurrency")
+            ]
+        ),
+        .testTarget(
+            name: "SwiftVoxAltaTests",
+            dependencies: [
+                "SwiftVoxAlta",
+                .product(name: "SwiftAcervo", package: "SwiftAcervo"),
+                .product(name: "Tuberia", package: "SwiftTuberia"),
+                .product(name: "VoxFormat", package: "vox-format"),
+            ],
+            swiftSettings: [
+                .enableUpcomingFeature("StrictConcurrency")
+            ]
+        ),
+        .testTarget(
+            name: "DigaTests",
+            dependencies: [
+                "diga",
+                "SwiftVoxAlta",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "SwiftAcervo", package: "SwiftAcervo"),
+                .product(name: "VoxFormat", package: "vox-format"),
+            ],
+            swiftSettings: [
+                .enableUpcomingFeature("StrictConcurrency")
+            ]
+        ),
+    ]
 )
